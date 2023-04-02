1cd141241999390cc33727da35f9888f5497a7d2
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-02 11:06:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './styles/App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
+  const [text, setText] = useState('');
+  const [radio, setRadio] = useState('');
+  const [checkbox, setCheckbox] = useState([false, false, false]);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleRadioChange = (event) => {
+    setRadio(event.target.value);
+  };
+
+  const handleCheckboxChange = (event) => {
+    const index = parseInt(event.target.value) - 1;
+    setCheckbox((prevCheckbox) => {
+      const newCheckbox = [...prevCheckbox];
+      newCheckbox[index] = !prevCheckbox[index];
+      return newCheckbox;
+    });
+  };
+
   return (
     <div className="container">
       <h1>React Controlled Forms</h1>
@@ -12,24 +34,55 @@ function App() {
             <label htmlFor="text" className="form-label">
               Example textarea
             </label>
-            <textarea className="form-control" id="text" rows="3" name="text"></textarea>
+            <textarea
+              className="form-control"
+              id="text"
+              rows="3"
+              name="text"
+              value={text}
+              onChange={handleTextChange}
+            ></textarea>
             <label className="form-check-label" htmlFor="radio">
               Default radio
             </label>
             <div className="form-check">
-              <input className="form-check-input" type="radio" value="1" id="radio1" name="radio" checked={radio === '1'} />
+              <input
+                className="form-check-input"
+                type="radio"
+                value="1"
+                id="radio1"
+                name="radio"
+                checked={radio === '1'}
+                onChange={handleRadioChange}
+              />
               <label className="form-check-label" htmlFor="radio1">
                 Radio 1
               </label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="radio" value="2" id="radio2" name="radio" checked={radio === '2'} />
+              <input
+                className="form-check-input"
+                type="radio"
+                value="2"
+                id="radio2"
+                name="radio"
+                checked={radio === '2'}
+                onChange={handleRadioChange}
+              />
               <label className="form-check-label" htmlFor="radio2">
                 Radio 2
               </label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="radio" value="3" id="radio3" name="radio" checked={radio === '3'} />
+              <input
+                className="form-check-input"
+                type="radio"
+                value="3"
+                id="radio3"
+                name="radio"
+                checked={radio === '3'}
+                onChange={handleRadioChange}
+              />
               <label className="form-check-label" htmlFor="radio3">
                 Radio 3
               </label>
@@ -38,19 +91,36 @@ function App() {
               Default checkbox
             </label>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="1" id="checkbox1" name="checkbox" checked={checkbox[0]} />
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="1"
+                id="checkbox1"
+                name="checkbox"
+                checked={checkbox[0]}
+                onChange={handleCheckboxChange}
+              />
               <label className="form-check-label" htmlFor="checkbox1">
                 Checkbox 1
               </label>
             </div>
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="2" id="checkbox2" name="checkbox" checked={checkbox[1]} />
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="2"
+                id="checkbox2"
+                name="checkbox"
+                checked={checkbox[1]}
+                onChange={handleCheckboxChange}
+              />
               <label className="form-check-label" htmlFor="checkbox2">
                 Checkbox 2
               </label>
             </div>
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="3" id="checkbox3" name="checkbox" checked={checkbox[2]} />
+             <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="3" id="checkbox3" name="checkbox" checked={checkbox[2]} onChange={handleCheckboxChange}
+/>
               <label className="form-check-label" htmlFor="checkbox3">
                 Checkbox 3
               </label>
@@ -60,13 +130,13 @@ function App() {
         <div className="col md-6">
           <h3>Form Output</h3>
           <p className="text">
-            <span className="title">Text : </span>
+            <span className="title">Text : </span>{text}
           </p>
           <p className="radio">
-            <span className="title">Radio : </span>
+            <span className="title">Radio : </span>{radio}
           </p>
           <p className="checkbox">
-            <span className="title">Checkbox : </span> {`[ `}
+            <span className="title">Checkbox : </span> {`[ `}{checkbox.join(", ")}
             {`]`}
           </p>
         </div>
@@ -76,3 +146,83 @@ function App() {
 }
 
 export default App;
+
+
+// import './styles/App.css';
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1>React Controlled Forms</h1>
+//       <div className="row">
+//         <div className="col md-6">
+//           <h3>Controlled Form</h3>
+//           <form>
+//             <label htmlFor="text" className="form-label">
+//               Example textarea
+//             </label>
+//             <textarea className="form-control" id="text" rows="3" name="text"></textarea>
+//             <label className="form-check-label" htmlFor="radio">
+//               Default radio
+//             </label>
+//             <div className="form-check">
+//               <input className="form-check-input" type="radio" value="1" id="radio1" name="radio" checked={radio === '1'} />
+//               <label className="form-check-label" htmlFor="radio1">
+//                 Radio 1
+//               </label>
+//             </div>
+//             <div className="form-check">
+//               <input className="form-check-input" type="radio" value="2" id="radio2" name="radio" checked={radio === '2'} />
+//               <label className="form-check-label" htmlFor="radio2">
+//                 Radio 2
+//               </label>
+//             </div>
+//             <div className="form-check">
+//               <input className="form-check-input" type="radio" value="3" id="radio3" name="radio" checked={radio === '3'} />
+//               <label className="form-check-label" htmlFor="radio3">
+//                 Radio 3
+//               </label>
+//             </div>
+//             <label className="form-check-label" htmlFor="checkbox">
+//               Default checkbox
+//             </label>
+//             <div className="form-check">
+//               <input className="form-check-input" type="checkbox" value="1" id="checkbox1" name="checkbox" checked={checkbox[0]} />
+//               <label className="form-check-label" htmlFor="checkbox1">
+//                 Checkbox 1
+//               </label>
+//             </div>
+//             <div className="form-check">
+//               <input className="form-check-input" type="checkbox" value="2" id="checkbox2" name="checkbox" checked={checkbox[1]} />
+//               <label className="form-check-label" htmlFor="checkbox2">
+//                 Checkbox 2
+//               </label>
+//             </div>
+//             <div className="form-check">
+//               <input className="form-check-input" type="checkbox" value="3" id="checkbox3" name="checkbox" checked={checkbox[2]} />
+//               <label className="form-check-label" htmlFor="checkbox3">
+//                 Checkbox 3
+//               </label>
+//             </div>
+//           </form>
+//         </div>
+//         <div className="col md-6">
+//           <h3>Form Output</h3>
+//           <p className="text">
+//             <span className="title">Text : </span>
+//           </p>
+//           <p className="radio">
+//             <span className="title">Radio : </span>
+//           </p>
+//           <p className="checkbox">
+//             <span className="title">Checkbox : </span> {`[ `}
+//             {`]`}
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
